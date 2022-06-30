f90a8ee4537117005812251edc3c060e9b19d479
Joshua Quizon <joshquizon@gmail.com>
2022-06-30 18:33:03 -0400

    authenticating path '/secure/**'

--- src/main/java/com/personalwebsite/personalwebsite/admin/config/SecurityConfig.java
+++ src/main/java/com/personalwebsite/personalwebsite/admin/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/secure/*", "/admin/*").authenticated()
+            .antMatchers("/secure/**").authenticated()
             .and()
             .httpBasic();
     }
--- src/main/java/com/personalwebsite/personalwebsite/admin/user/controller/UserController.java
+++ src/main/java/com/personalwebsite/personalwebsite/admin/user/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin/users")
+@RequestMapping("/secure/admin/users")
 @RequiredArgsConstructor
 public class UserController {
 
