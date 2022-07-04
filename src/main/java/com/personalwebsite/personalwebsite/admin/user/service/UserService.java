422909e849aab49ca0a0efa6d04905299b034b4a
Joshua Quizon <joshquizon@gmail.com>
2022-07-03 20:30:01 -0400

    basic security measures implemented, more in comment

--- src/main/java/com/personalwebsite/personalwebsite/admin/user/service/UserService.java
+++ src/main/java/com/personalwebsite/personalwebsite/admin/user/service/UserService.java
@@ -2,19 +2,25 @@ package com.personalwebsite.personalwebsite.admin.user.service;
 
 
 import com.personalwebsite.personalwebsite.admin.user.dao.UserDao;
+import com.personalwebsite.personalwebsite.admin.user.pojo.Role;
 import com.personalwebsite.personalwebsite.admin.user.pojo.User;
 import com.personalwebsite.personalwebsite.service.ServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserService implements ServiceInterface<User> {
+public class UserService implements ServiceInterface<User>, UserDetailsService {
 
     private final UserDao userDao;
 
@@ -28,13 +34,35 @@ public class UserService implements ServiceInterface<User> {
         return userDao.findAll();
     }
 
+    public User get(String username) { return userDao.findByUsername(username); }
+
     @Override
     public User get(Long id) {
         return userDao.findById(id);
     }
 
-    public User get(String username){
-        return userDao.findByUsername(username);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userDao.findByUsername(username);
+
+        if(user == null){
+            System.out.println("User was not found in the database");
+            throw new UsernameNotFoundException("User not found.");
+        }
+        else{
+            System.out.println("User exists in the database");
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(Role r : user.getRoles()){
+            authorities.add(new SimpleGrantedAuthority(r.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
     }
 
     @Override
