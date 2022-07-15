188bba9b5c7d797931340a0cd4d9ba6620b76ec4
Joshua Quizon <joshquizon@gmail.com>
2022-07-15 11:23:33 -0400

    AWS is now setup and in use, also made jdbc template constructor a bean

--- src/main/java/com/personalwebsite/personalwebsite/db/DbConfiguration.java
+++ src/main/java/com/personalwebsite/personalwebsite/db/DbConfiguration.java
@@ -1,6 +1,7 @@
 package com.personalwebsite.personalwebsite.db;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,6 +19,7 @@ public class DbConfiguration {
     @Value("${spring.datasource.password}")
     String password;
 
+    @Bean
     public JdbcTemplate jdbcTemplate(){
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
