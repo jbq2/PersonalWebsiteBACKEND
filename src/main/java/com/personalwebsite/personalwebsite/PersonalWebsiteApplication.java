7da87fc61b9d7ace0b84a4b2a9d3052d961b8458
Joshua Quizon <joshquizon@gmail.com>
2022-07-16 20:37:19 -0400

    removing Allow-Access-Control-Origin header

--- src/main/java/com/personalwebsite/personalwebsite/PersonalWebsiteApplication.java
+++ src/main/java/com/personalwebsite/personalwebsite/PersonalWebsiteApplication.java
@@ -28,7 +28,7 @@ public class PersonalWebsiteApplication {
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+                "Access-Control-Allow-Credentials", "Filename"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
