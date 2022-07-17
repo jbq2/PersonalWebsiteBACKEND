3c39459bb5108068d3332db41fd79a60cbdf6bd9
Joshua Quizon <joshquizon@gmail.com>
2022-07-17 11:43:24 -0400

    changing deployed origin link to https

--- src/main/java/com/personalwebsite/personalwebsite/admin/config/CorsConfig.java
+++ src/main/java/com/personalwebsite/personalwebsite/admin/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://jbq2-portfolio.herokuapp.com", "http://localhost:4200")
+                .allowedOrigins("https://jbq2-portfolio.herokuapp.com", "http://localhost:4200")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(-1);
