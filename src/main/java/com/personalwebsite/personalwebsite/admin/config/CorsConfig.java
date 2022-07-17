99a33231a533417a93ffddb36ac6f29fb05e4ab6
Joshua Quizon <joshquizon@gmail.com>
2022-07-17 11:34:55 -0400

    adding deployed angular app origin

--- src/main/java/com/personalwebsite/personalwebsite/admin/config/CorsConfig.java
+++ src/main/java/com/personalwebsite/personalwebsite/admin/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:4200", "http://jbq2-portfolio.herokuapp.com")
+                .allowedOrigins("http://jbq2-portfolio.herokuapp.com", "http://localhost:4200")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(-1);
