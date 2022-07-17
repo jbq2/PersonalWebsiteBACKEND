64c9db3af2478a21368560adf9f5fbfbfa18787b
Joshua Quizon <joshquizon@gmail.com>
2022-07-16 20:34:38 -0400

    Deployment Cors

--- src/main/java/com/personalwebsite/personalwebsite/PersonalWebsiteApplication.java
+++ src/main/java/com/personalwebsite/personalwebsite/PersonalWebsiteApplication.java
@@ -24,7 +24,7 @@ public class PersonalWebsiteApplication {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
